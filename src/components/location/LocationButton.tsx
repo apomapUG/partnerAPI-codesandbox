4d579b22f801714f9101dcb15bc456c4a849ad17
Damilola Bada <badadamilola@gmail.com>
2023-01-05 17:04:54 +0100

    Delete src directory

--- src/components/location/LocationButton.tsx
+++
@@ -1,9 +0,0 @@
-import { GoPrimitiveDot } from "react-icons/go";
-
-type LocationButtonProps = {
-  locationColor: string;
-};
-
-export default function LocationButton({ locationColor }: LocationButtonProps) {
-  return <GoPrimitiveDot style={{ fill: locationColor }} />;
-}
--- src/types/Location.ts
+++
@@ -1,9 +0,0 @@
-export type Location = {
-  address: string
-  id: string
-  name: string
-  owner: string
-  system: string
-  type: string
-  color: string
-  };
